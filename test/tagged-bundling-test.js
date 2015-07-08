805da42f662caa5af9329f999fd7cd444e2182e7
Kristian Mandrup <kmandrup@gmail.com>
2015-07-08 01:11:06 +0100

    almost got the tags

--- test/tagged-bundling-test.js
+++ test/tagged-bundling-test.js
@@ -22,50 +22,94 @@ describe('lasso/bundling', function() {
         });
         done();
     });
-    it('should bundle the bundles matching page tags', function(done) {
+    // it('should bundle the bundles matching page tags', function(done) {
+    //     var lasso = require('../');
+    //     var theLasso = lasso.create({
+    //         fileWriter: {
+    //             outputDir: outputDir,
+    //             fingerprintsEnabled: false
+    //         },
+    //         bundles: [
+    //             {
+    //                 name: 'bar',
+    //                 tags: ['bar'],
+    //                 dependencies: [
+    //                     './bar-widgets.js'
+    //                 ]
+    //             },
+    //             {
+    //                 name: 'foo',
+    //                 tags: ['foo', 'live'],
+    //                 dependencies: [
+    //                     './foo-widgets.js'
+    //                 ]
+    //             }                
+    //         ]
+    //     }, nodePath.join(__dirname, 'test-tag-bundling-page'), __filename);
+
+    //     var writerTracker = require('./WriterTracker').create(theLasso.writer);
+    //     theLasso.lassoPage({
+    //             pageName: 'testTagBundlingPage',
+    //             dependencies: [
+    //                     'require: ./my-tagged-page'
+    //                 ],
+    //             from: nodePath.join(__dirname, 'test-tag-bundling-page')
+    //         },
+    //         function(err, lassoPageResult) {
+    //             if (err) {
+    //                 return done(err);
+    //             }
+
+    //             var code = writerTracker.getCodeForFilename('my-tagged-page.js');
+    //             expect(code).to.not.contain('[MAIN]');
+    //             expect(code).to.not.contain('[BAR]');
+    //             expect(code).to.contain('[FOO-WIDGETS]');
+    //             done();
+    //         });
+
+    // });
+
+    it('should allow for optimizing a page with tags and bundles', function(done) {
         var lasso = require('../');
-        var theLasso = lasso.create({
+
+        var from = nodePath.join(__dirname, 'test-tags-project');
+        var myLasso = lasso.create({
             fileWriter: {
                 outputDir: outputDir,
+                urlPrefix: '/',
                 fingerprintsEnabled: false
             },
+            tags: ['foo'],
+            bundlingEnabled: true,
+
             bundles: [
                 {
-                    name: 'bar',
-                    tags: ['bar'],
-                    dependencies: [
-                        './bar-widgets.js'
-                    ]
-                },
-                {
                     name: 'foo',
-                    tags: ['foo', 'live'],
+                    tags: ['foo'],
                     dependencies: [
-                        './foo-widgets.js'
+                        './browser.json'
                     ]
-                }                
-            ]
-        }, nodePath.join(__dirname, 'test-bundling-project'), __filename);
-
-        var writerTracker = require('./WriterTracker').create(theLasso.writer);
-        theLasso.lassoPage({
-                pageName: 'testTagBundlingPage',
-                dependencies: [
-                        'require: ./my-tagged-page'
-                    ],
-                from: nodePath.join(__dirname, 'test-tag-bundling-page')
-            },
-            function(err, lassoPageResult) {
-                if (err) {
-                    return done(err);
                 }
+            ]
+        }, from, __filename);
+        var writerTracker = require('./WriterTracker').create(myLasso.writer);
+        myLasso.lassoPage({
+                pageName: 'testPage',
+                tags: ['foo'],
+                dependencies: [
+                    './c.js'
+                ],
+                from: from
+            })
+            .then(function(lassoPageResult) {
 
-                var code = writerTracker.getCodeForFilename('my-tagged-page.js');
-                expect(code).to.not.contain('[MAIN]');
-                expect(code).to.not.contain('[BAR]');
-                expect(code).to.contain('[FOO-WIDGETS]');
-                done();
-            });
-        });        
+                expect(writerTracker.getOutputFilenames()).to.deep.equal([
+                    'foo.js'
+                ]);
+
+                expect(writerTracker.getCodeForFilename('foo.js')).to.equal('a=true;\nc=true;');
+                lasso.flushAllCaches(done);
+            })
+            .done();
     });
 });    
